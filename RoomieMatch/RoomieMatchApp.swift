1c06097b2b17ece85dacf72dd406097fb06a20b9
Preet Karia <karia.preet5@gmail.com>
2023-03-29 17:01:04 -0400

    Initial Commit

---
+++ RoomieMatch/RoomieMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomieMatchApp.swift
+//  RoomieMatch
+//
+//  Created by Preet Karia on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct RoomieMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
